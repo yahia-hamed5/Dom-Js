b0e3d489cdd100c618ee503c3410a2e9f7c0279b
yahia hamed <yahiahamed2411@gmail.com>
2025-11-09 17:59:22 +0200

    Create Modal Window

---
+++ Modal_Window/app.js
@@ -0,0 +1,23 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+const closeModel = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+btnsOpenModal.forEach((el) => {
+  el.addEventListener("click", openModal);
+});
+btnCloseModal.addEventListener("click", closeModel);
+overlay.addEventListener("click", closeModel);
+
+document.addEventListener('keydown',function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+            closeModel();
+    }
+})
