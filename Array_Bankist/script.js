ffb03515f652a9fb82b4b823adf91c47760eae1c
yahia hamed <yahiahamed2411@gmail.com>
2026-01-08 16:50:03 +0200

    array methods practes

--- Array_Bankist/script.js
+++ Array_Bankist/script.js
@@ -112,32 +112,36 @@ createUserNames(accounts);
 console.log(accounts);
 
 // event handlers login
-let currentAccount
-btnLogin.addEventListener('click',(e)=>{
+let currentAccount;
+btnLogin.addEventListener("click", (e) => {
   e.preventDefault();
   const username = inputLoginUsername.value;
-  const pin = Number (inputLoginPin.value);
-  currentAccount = accounts.find(acc => acc.username === username && acc.pin === pin);
-  if (currentAccount?.pin === pin){
+  const pin = Number(inputLoginPin.value);
+  currentAccount = accounts.find(
+    (acc) => acc.username === username && acc.pin === pin
+  );
+  if (currentAccount?.pin === pin) {
     containerApp.style.opacity = 100;
-    labelWelcome.textContent = `Welcome Back , ${currentAccount.owner.split(' ')[0 ]}`;
-    inputLoginUsername.value = inputLoginPin.value = '';
+    labelWelcome.textContent = `Welcome Back , ${
+      currentAccount.owner.split(" ")[0]
+    }`;
+    inputLoginUsername.value = inputLoginPin.value = "";
     inputLoginPin.blur();
     displayMovments(currentAccount.movements);
     displaybalance(currentAccount.movements);
     calcDisplaySummary(currentAccount);
   }
-})
-
-// transfer money 
-btnTransfer.addEventListener('click',(e)=>{
-  e.preventDefault();
-  const amount = Number (inputTransferAmount.value);
-  const recieverAcc = accounts.find(acc => acc.username === inputTransferTo.value);
-  console.log(amount,recieverAcc);
 });
 
-
+// transfer money
+btnTransfer.addEventListener("click", (e) => {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const recieverAcc = accounts.find(
+    (acc) => acc.username === inputTransferTo.value
+  );
+  console.log(amount, recieverAcc);
+});
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -296,7 +300,10 @@ GOOD LUCK 😀
 const calcAverageHumanAge = (ages) => {
   const humanAges = ages.map((age) => (age <= 2 ? 2 * age : 16 + age * 4));
   const adults = humanAges.filter((age) => age >= 18);
-  const average = adults.reduce((acc, age, i, arr) => acc + age / arr.length,0);
+  const average = adults.reduce(
+    (acc, age, i, arr) => acc + age / arr.length,
+    0
+  );
   return average;
 };
 
@@ -309,11 +316,98 @@ console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
 // TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 
 // GOOD LUCK 😀
-const calcAverageHumanAgeArrowChaning = (ages) =>  ages
+const calcAverageHumanAgeArrowChaning = (ages) =>
+  ages
     .map((age) => (age <= 2 ? 2 * age : 16 + age * 4))
     .filter((age) => age >= 18)
     .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
-;
+console.log(calcAverageHumanAgeArrowChaning([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAgeArrowChaning([16, 6, 10, 5, 6, 1, 4]));
 
-console.log(calcAverageHumanAgeArrowChaning([5, 2, 4, 1, 15, 8, 3]))
-console.log(calcAverageHumanAgeArrowChaning([16, 6, 10, 5, 6, 1, 4]))
+// Coding Challenge #5
+
+/* 
+Julia and Kate are still studying dogs. This time they are want to figure out if the dogs in their are eating too much or too little food.
+
+- Formula for calculating recommended food portion: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+- Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
+- Eating an okay amount means the dog's current food portion is within a range 10% above and below the recommended portion (see hint).
+
+YOUR TASKS:
+1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion (recFood) and add it to the object as a new property. Do NOT create a new array, simply loop over the array (We never did this before, so think about how you can do this without creating a new array).
+2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple users, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+3. Create an array containing all owners of dogs who eat too much (ownersTooMuch) and an array with all owners of dogs who eat too little (ownersTooLittle).
+4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+5. Log to the console whether there is ANY dog eating EXACTLY the amount of food that is recommended (just true or false)
+6. Log to the console whether ALL of the dogs are eating an OKAY amount of food (just true or false)
+7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+8. Group the dogs into the following 3 groups: 'exact', 'too-much' and 'too-little', based on whether they are eating too much, too little or the exact amount of food, based on the recommended food portion.
+9. Group the dogs by the number of owners they have
+10. Sort the dogs array by recommended food portion in an ascending order. Make sure to NOT mutate the original array!
+
+HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
+HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
+
+TEST DATA:
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John', 'Leo'] },
+  { weight: 18, curFood: 244, owners: ['Joe'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+GOOD LUCK 😀
+*/
+
+console.log("----- challenge 5 -----");
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ["Alice", "Bob"] },
+  { weight: 8, curFood: 200, owners: ["Matilda"] },
+  { weight: 13, curFood: 275, owners: ["Sarah", "John", "Leo"] },
+  { weight: 18, curFood: 244, owners: ["Joe"] },
+  { weight: 32, curFood: 340, owners: ["Michael"] },
+];
+
+dogs.forEach((dog) => {
+  dog.recFood = Math.trunc(dog.weight ** 0.75 * 28);
+});
+console.log(dogs);
+// ===============================================
+const sarahDog = dogs.find((dog) => dog.owners.includes("Sarah"));
+console.log(
+  `Sarah's dog is  eating too ` +
+    `${sarahDog.curFood > sarahDog.recFood ? "much" : "little"}`
+);
+// ===============================================
+const ownersTooMuch = dogs.filter((dog)=> dog.curFood > dog.recFood).flatMap((dog)=>dog.owners)
+console.log(ownersTooMuch);
+const ownersTooLittle = dogs.filter((dog)=> dog.curFood < dog.recFood).flatMap((dog)=>dog.owners)
+console.log(ownersTooLittle);
+// ===============================================
+
+ console.log(`${ownersTooMuch.join(' and ')}'s dogs eat too much!`)
+ console.log(`${ownersTooLittle.join(' and ')}'s dogs eat too little!`)
+
+// ===============================================
+console.log(dogs.some(dog => dog.curFood > (dog.recFood * 0.9) && dog.curFood < (dog.recFood * 1.1) ? true : false));
+// ===============================================
+console.log(dogs.every(dog => dog.curFood > (dog.recFood * 0.9) && dog.curFood < (dog.recFood * 1.1) ? true : false));
+// ===============================================
+console.log(dogs.filter(dog => dog.curFood > (dog.recFood * 0.9) && dog.curFood < (dog.recFood * 1.1) ? true : false));
+
+// ===============================================
+
+const dogsSorted = Object.groupBy(dogs,dog =>
+dog.curFood > (dog.recFood * 0.9) && dog.curFood < (dog.recFood * 1.1) ? 'exact' : dog.curFood > dog.recFood ? 'too-much' : 'too-little'
+)
+console.log(dogsSorted); 
+// ===============================================
+const dogsByOwners = Object.groupBy(dogs,dog => dog.owners.length);
+console.log(dogsByOwners);
+// ===============================================
+
+
+const result = 0 == -null;
+console.log(result);
