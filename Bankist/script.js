9da88e42790f0e535fc7b6d46fc6f0f779c764fa
yahia hamed <yahiahamed2411@gmail.com>
2026-02-05 19:57:35 +0200

    create new bankist app

---
+++ Bankist/script.js
@@ -0,0 +1,85 @@
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const header = document.querySelector('.header');
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+// console.log(document.head);
+// console.log(document.body);
+
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// message.innerHTML = `We Use cookied for improved functionality and analytics. <button class="btn btn-close-cookie">Got it!</button>`;
+
+// header.append(message);
+
+// document.querySelector('.btn-close-cookie').addEventListener('click',function (){
+//   message.parentElement.removeChild(message);
+// })
+
+// message.style.backgroundColor="#37383d";
+// message.style.width="120%";
+// console.log(getComputedStyle(message).height)
+// console.log(getComputedStyle(message).backgroundColor)
+// message.style.height = Number.parseFloat(getComputedStyle(message).height,10) + 30 + 'px'
+
+// document.documentElement.style.setProperty('--color-primary','orangered')
+
+// const logo = document.querySelector('.nav__logo')
+// console.log(logo.alt)
+// console.log(logo.getAttribute('designer'))
+// logo.setAttribute('company','bankist')
+// console.log(logo.getAttribute('company'))
+// console.log(logo.dataset.id)
+
+// logo.classList.add()
+// logo.classList.remove()
+// logo.classList.toggle()
+// logo.classList.contains()
+
+// -------------------------------
+
+// implementing smoth scrollings
+const btnScrollTo =document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+btnScrollTo.addEventListener('click',function (e) {
+  // console.log('current scroll x/y',pageXOffset,pageYOffset)
+  // console.log('current scroll x/y',document.documentElement.clientHeight,document.documentElement.clientWidth)
+  // const s1scored = section1.getBoundingClientRect()
+  // window.scrollTo({
+  //   left : s1scored.left + window.pageXOffset,
+  //   top : s1scored.top + window.pageYOffset,
+  //   behavior:'smooth',
+  // });
+  section1.scrollIntoView({behavior:'smooth'})
+})
+
+// type events
+
+// one  mouseenter
+// tow  mouseenter
+// three  mouseenter
+// four  mouseenter
+// five  mouseenter
