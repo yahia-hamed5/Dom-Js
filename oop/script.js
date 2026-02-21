e159452599ddce60bfe36a76d1dad0b7d07a5ccf
yahia hamed <yahiahamed2411@gmail.com>
2026-02-21 14:36:42 +0200

    edit

---
+++ oop/script.js
@@ -0,0 +1,12 @@
+console.log('The first OOP')
+
+/*
+=> How can i Create a new Class
+ #there are some Rules 
+ 1=>abstraction --- التجريد
+ 1=>Encapsulation ------ التغليف
+ 1=>Inheritance --- الوراثه
+ 1=>Polymorphism ---- تعدد الاشكال
+*/
+
+// 1) Abstraction 
